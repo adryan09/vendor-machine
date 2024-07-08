835fcab4313d1556a0d0c7cf0d463240155a91a5
adryan09 <adrian.ghita09@gmail.com>
2024-07-09 00:27:35 +0300

    Update AddCredit.js

--- src/components/EnterMoney/AddCredit.js
+++ src/components/EnterMoney/AddCredit.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Coin from './Coin';
 import { Wrapper, } from './style';
 
-
+const coinList = ['5','10', '20', '30', '50'];
 const AddCredit = () => {
     return <Wrapper>
         {
-            ['5','10', '20', '30', '50'].map((v, i) => (<div key={i}>
+            coinList.map((v, i) => (<div key={i}>
         <Coin idx={i} coinValue={v}/>
     </div>))
         }
